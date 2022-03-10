b0671d327a2278b02f5229a32f29169b23cfafcd
Franck MARTINEZ <37777086+Pacokris@users.noreply.github.com>
2022-03-10 16:01:08 +0100

    Update wsgi.py

--- fir/wsgi.py
+++ fir/wsgi.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.config.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.config.composeprod")
 
 application = get_wsgi_application()
