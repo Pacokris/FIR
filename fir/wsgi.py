1dfeb7bcd7229660e46ab8369089da8b8d72d48d
Franck MARTINEZ <37777086+Pacokris@users.noreply.github.com>
2022-03-10 15:01:32 +0100

    Update wsgi.py

--- fir/wsgi.py
+++ fir/wsgi.py
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.config.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fir.heroku_settings")
 
 application = get_wsgi_application()
